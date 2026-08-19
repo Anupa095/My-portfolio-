09497b2dbc403de13b37f8f72946ac7d922f8bf4
Anupa095 <anupaperera95@gmail.com>
2026-08-19 15:34:30 +0530

    Initial commit

---
+++ src/components/Skills.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Skills() {
+  return (
+    <section id="skills">
+      <div className="container">
+        <h2>My Skills</h2>
+        <ul>
+          <li>React JS</li>
+          <li>JavaScript</li>
+          <li>HTML & CSS</li>
+          <li>Node JS</li>
+          <li>MySQL</li>
+        </ul>
+      </div>
+    </section>
+  );
+}
+
+export default Skills;
